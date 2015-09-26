7c51f1923826982226828ab823a25a3b2c57d45e
Alexander Huang <alex@dhcp-18-111-24-86.dyn.mit.edu>
2015-09-26 14:40:58 -0400

    Added simple todo list

--- Client/controllers/todoctrl.js
+++ Client/controllers/todoctrl.js
@@ -1,4 +1,32 @@
 var myApp = angular.module('todoApp', []);
 myApp.controller('AppCtrl', function ($scope, $http) {
-  $scope.todoLists = [{},{}];
+
+  $scope.todoList = {};
+  $scope.item = {};
+  refresh();
+
+  $scope.addItem = function () {
+    $scope.item.time = (new Date()).getTime();
+    console.log($scope.item)
+    $http.post('/todolist', $scope.item).success(function (res) {
+      //console.log(res);
+      refresh();
+    })
+  };
+
+  $scope.remove = function (id) {
+    console.log("Telling the server to remove: " + id);
+    $http.delete("/todolist/" + id).success(function (res) {
+      refresh();
+    });
+  }
+
+  function refresh() {
+    $scope.item.checked = false;
+    $scope.item.name = "";
+    $http.get('/todolist').success(function (res) {
+      console.log(res);
+      $scope.todoList = res;
+    });
+  }
 });
--- server.js
+++ server.js
@@ -7,12 +7,23 @@ var io = require('socket.io')(http);
 var mg = require('mongoose');
 mg.connect('mongodb://admin:h32isawesome@ds041432.mongolab.com:41432/checklist');
 
-var Item = mg.model("item", {time:Number, checked:Boolean,name:String,claimed:Boolean,claimedby:String});
+var Item = mg.model("item", {
+  time: Number,
+  checked: Boolean,
+  name: String,
+  claimed: Boolean,
+  claimedby: String
+});
+
+var TodoItem = mg.model("todo_item", {
+  checked: Boolean,
+  name: String
+})
 
 app.use(express.static(__dirname + '/Client'));
 app.use(bodyParser.json());
 
-io.on('connection', function(socket){
+io.on('connection', function (socket) {
   console.log('a user connected');
 });
 
@@ -23,83 +34,151 @@ function refreshClients() {
 }
 
 function addItem(jsonItem, res) {
-  var checklistItem = new Item ({time: jsonItem.time, checked: jsonItem.check, name:jsonItem.name, claimed:jsonItem.claim, claimedby:jsonItem.claimby});
-  checklistItem.save(function(err, userObj){
-    if(err) console.log(err);
+  var checklistItem = new Item({
+    time: jsonItem.time,
+    checked: jsonItem.check,
+    name: jsonItem.name,
+    claimed: jsonItem.claim,
+    claimedby: jsonItem.claimby
+  });
+  checklistItem.save(function (err, userObj) {
+    if (err) console.log(err);
     else {
-      console.log("Saved "+userObj);
+      console.log("Saved " + userObj);
       res.json(userObj);
       refreshClients();
     }
   });
 }
 
-var checkItem= function(id,status,res) {
-  Item.findByIdAndUpdate(id,{$set: {checked: status}}, function (err,item) {
-    if(err) return console.error(err);
+function addTodoItem(jsonItem, res) {
+  var todolistItem = new TodoItem({
+    time: jsonItem.time,
+    checked: jsonItem.checked,
+    name: jsonItem.name
+  });
+  todolistItem.save(function (err, userObj) {
+    if (err) console.log(err);
+    else {
+      console.log("Saved " + userObj);
+      res.json(userObj);
+    }
+  });
+}
+
+var checkItem = function (id, status, res) {
+  Item.findByIdAndUpdate(id, {
+    $set: {
+      checked: status
+    }
+  }, function (err, item) {
+    if (err) return console.error(err);
     res.send(item);
     refreshClients();
   });
-}; 
+};
 
-var claimItem = function(id,status,name,res) {
-  if(status=="true") {
-    Item.findByIdAndUpdate(id,{$set: {claimed:status, claimedby:name}}, function (err,item) {
-    if(err) return console.error(err);
-    res.send(item);
-    refreshClients();
+var claimItem = function (id, status, name, res) {
+  if (status == "true") {
+    Item.findByIdAndUpdate(id, {
+      $set: {
+        claimed: status,
+        claimedby: name
+      }
+    }, function (err, item) {
+      if (err) return console.error(err);
+      res.send(item);
+      refreshClients();
     });
   } else {
-    Item.findByIdAndUpdate(id,{$set: {claimed:status, claimedby:""}}, function (err,item) {
-    if(err) return console.error(err);
-    res.send(item);
-    refreshClients();
+    Item.findByIdAndUpdate(id, {
+      $set: {
+        claimed: status,
+        claimedby: ""
+      }
+    }, function (err, item) {
+      if (err) return console.error(err);
+      res.send(item);
+      refreshClients();
     });
   }
 };
 
-app.delete("/itemlist/:id", function(req,res) {
+app.delete("/itemlist/:id", function (req, res) {
   var id = req.params.id;
   console.log("\nDeleting:" + id);
-  Item.remove({_id:id}, function(err) {
-    if(err) console.error(err);
+  Item.remove({
+    _id: id
+  }, function (err) {
+    if (err) console.error(err);
     //Sketchy
     else res.send("Removed");
     refreshClients();
   });
 });
 
-app.post("/itemcheck/:id/:status", function(req,res) {
+app.post("/itemcheck/:id/:status", function (req, res) {
   var id = req.params.id;
   var status = req.params.status;
   console.log("\nGot POST request, un/checking item: " + id);
   checkItem(id, status, res);
 });
 
-app.post("/itemclaim/:id/:status/:name",function(req,res) {
+app.post("/itemclaim/:id/:status/:name", function (req, res) {
   var id = req.params.id;
   var status = req.params.status;
   var name = req.params.name;
   console.log("\n Got POST request, un/claiming item: " + id);
-  claimItem(id,status,name,res);
+  claimItem(id, status, name, res);
 });
 
-app.post("/itemlist", function(req, res) {
+app.post("/itemlist", function (req, res) {
   console.log("\nGot POST request, adding item");
   console.log(req.body);
   console.log("---------------");
   console.log(req.headers);
-  addItem(req.body,res);
+  addItem(req.body, res);
 });
 
-app.get("/itemlist", function(req, res) {
+app.get("/itemlist", function (req, res) {
   console.log("\nGot GET request, returning docs");
-  Item.find().sort({time:-1}).exec(function(err, items) {
-    if(err) return console.error(err);
+  Item.find().sort({
+    time: -1
+  }).exec(function (err, items) {
+    if (err) return console.error(err);
     console.log("Current database: " + items);
     res.json(items);
-  }
-  );  
+  });
+});
+
+app.get("/todolist", function (req, res) {
+  TodoItem.find().sort({
+    time: -1
+  }).exec(function (err, items) {
+    if (err) return console.error(err);
+    console.log("Current database: " + items);
+    res.json(items);
+  });
+});
+
+app.post("/todolist", function (req, res) {
+  console.log("\nGot POST request, adding item");
+  console.log(req.body);
+  console.log("---------------");
+  addTodoItem(req.body, res);
+});
+
+app.delete("/todolist/:id", function (req, res) {
+  var id = req.params.id;
+  console.log("\nDeleting:" + id);
+  TodoItem.remove({
+    _id: id
+  }, function (err) {
+    if (err) console.error(err);
+    //Sketchy
+    else res.send("Removed");
+    refreshClients();
+  });
 });
 
 http.listen(3000);
